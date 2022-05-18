64219cb0e2bd06bf3f1e36580fe6398eb399c0b2
Savani Kanhere <ksavani19@gmail.com>
2022-05-18 13:50:25 +0530

    updated settings static root

--- Disease/settings.py
+++ Disease/settings.py
@@ -148,22 +148,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-#commented for deployment
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'Health/static/'),
+    os.path.join(BASE_DIR, 'patient/static/')
+]
 
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS =[
-#    os.path.join(BASE_DIR, 'Health/static/'),
-#    os.path.join(BASE_DIR, 'patient/static/')
-#]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#umcommented for deployment
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
