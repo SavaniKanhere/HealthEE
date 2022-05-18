f2ebba243947b435ab88c807446f831609314056
Savani Kanhere <ksavani19@gmail.com>
2022-05-18 13:44:44 +0530

    updated settings, requirements, deleted media

--- Disease/settings.py
+++ Disease/settings.py
@@ -148,20 +148,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+#commented for deployment
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS =[
+#    os.path.join(BASE_DIR, 'Health/static/'),
+#    os.path.join(BASE_DIR, 'patient/static/')
+#]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#umcommented for deployment
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'Health/static/'),
-    os.path.join(BASE_DIR, 'patient/static/')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
